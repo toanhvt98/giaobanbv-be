623e55c0a99d5874a92ae3163c7a3e981386040f
kiendt <dotrungkien6987@gmail.com>
2023-09-14 13:59:44 +0700

    sua limmit user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -60,7 +60,7 @@ userController.getUsers = catchAsync(async (req, res, next) => {
   let { page, limit, ...filter } = { ...req.query };
 
   page = parseInt(page) || 1;
-  limit = parseInt(limit) || 10;
+  limit = parseInt(limit) || 100;
 
   const filterConditions = [];
 
